ca656f2dd04e9bf0f20cf1db5ee73955d3a46673
matthegg5 <matthew.heggie@googlemail.com>
2015-07-24 19:49:27 +0100

    commit message

---
+++ OpenMES-master/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenMES_master
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            //hello world
+        }
+    }
+}
